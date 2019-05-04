9c5b434ac384ff8a7df8b25495df84d09e6a247f
Robert Turner Jr <georob23@gmail.com>
2019-05-03 21:37:09 -0600

    Add Move struct and logic.

--- rust_wasm/rustycheckers/src/board.rs
+++ rust_wasm/rustycheckers/src/board.rs
@@ -26,3 +26,66 @@ impl GamePiece {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Coordinate(pub usize, pub usize); // Tuple struct. Default accessors are .0 and .1
+impl Coordinate {
+    pub fn on_board(self) -> bool {
+        let Coordinate(x, y) = self;  // Destructuring to pull x and y out
+        x <= 7 && y <= 7
+    }
+
+    // Produce an iterator of Coordinates of all possible jumps from given Coordinate
+    pub fn jump_targets_from(&self) -> impl Iterator<Item = Coordinate> {
+        let mut jumps = Vec::new();
+        let Coordinate(x, y) = *self;
+
+        if y >= 2 {
+            jumps.push(Coordinate(x + 2, y - 2));
+        }
+        jumps.push(Coordinate(x + 2, y - 2));
+
+        if x >= 2 && y >= 2 {
+            jumps.push(Coordinate(x - 2, y - 2));
+        }
+
+        if x >= 2 {
+            jumps.push(Coordinate(x - 2, y + 2));
+        }
+        jumps.into_iter()
+    }
+
+    // Produce an iterator of Coordinates of all possible moves from given Coordinate
+    pub fn move_targets_from(&self) -> impl Iterator<Item = Coordinate> {
+        let mut moves = Vec::new();
+        let Coordinate(x, y) = *self;
+
+        if x >= 1 {
+            moves.push(Coordinate(x - 1, y + 1));
+        }
+        moves.push(Coordinate(x + 1, y + 1));
+
+        if y >= 1 {
+            moves.push(Coordinate(x +1, y - 1));
+        }
+
+        if x >= 1 && y >= 1 {
+            moves.push(Coordinate(x - 1, y - 1));
+        }
+        moves.into_iter()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct Move {
+    pub from: Coordinate,
+    pub to:   Coordinate,
+}
+impl Move{
+    pub fn new(from: (usize, usize), to: (usize, usize)) -> Move {
+        Move {
+            from: Coordinate(from.0, from.1),
+            to:   Coordinate(to.0, to.1),
+        }
+    }
+}
