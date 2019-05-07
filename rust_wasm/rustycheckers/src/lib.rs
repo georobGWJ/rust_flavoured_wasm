803ca515b651d50b8bd1c6870c5c075bc74670d4
Robert Turner Jr <georob23@gmail.com>
2019-05-06 21:23:47 -0600

    Add mut_static and lazy_static libraries.

--- rust_wasm/rustycheckers/src/lib.rs
+++ rust_wasm/rustycheckers/src/lib.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate lazy_static;
+
+// lazy_static allows creation of a globally available instance of
+// the GameEngine struct. 
+use board::{Coordinate, GamePiece, Move, PieceColor};
+use game::GameEngine;
+use mut_static::MutStatic;
+
+lazy_static! {
+    pub static ref GAME_ENGINE: MutStatic<GameEngine> =
+        { MutStatic::from(GameEngine::new()) };
+}
+
 mod board;
 mod game;
 
