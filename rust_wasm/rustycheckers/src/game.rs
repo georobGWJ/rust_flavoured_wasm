503251ae63551c11e7150bd20784306b4a34772a
Robert Turner Jr <georob23@gmail.com>
2019-05-07 19:57:52 -0600

    Add many helper functions to game logic.

--- rust_wasm/rustycheckers/src/game.rs
+++ rust_wasm/rustycheckers/src/game.rs
@@ -68,6 +68,40 @@ impl GameEngine {
         })
     }
 
+    pub fn get_piece(&self, coord: Coordinate) -> Result<Option<GamePiece>, ()> {
+        let Coordinate(x, y) = coord;
+        if x <= 7 && y <=7 {
+            Ok(self.board[x][y])
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn current_turn(&mut self) -> PieceColor {
+        self.current_turn
+    }
+
+    pub fn is_crowned(&self, coord: Coordinate) -> bool {
+        let Coordinate(x, y) = coord;
+        match self.board[x][y] {
+            Some(piece) => piece.crowned,
+            None => false,
+        }
+    }
+
+    pub fn move_count(&self) -> u32 {
+        self.move_count
+    }
+
+    fn advance_turn(&mut self) {
+        if self.current_turn == PieceColor::Black {
+            self.current_turn = PieceColor::White
+        } else {
+            self.current_turn = PieceColor::Black
+        }
+        self.move_count += 1;
+    }
+
     fn legal_moves(&self) -> Vec<Move> {
         let mut moves: Vec<Move> = Vec::new();
 
@@ -109,6 +143,91 @@ impl GameEngine {
             Vec::new()
         }
     }
+
+    fn valid_move(&self, p: &GamePiece, from: &Coordinate, to: &Coordinate) -> bool {
+        if !to.on_board() || !from.on_board() {
+            false
+        } else {
+            let Coordinate(tx, ty) = *to;
+            if let Some(_piece) = self.board[tx][ty] {
+                false
+            } else {
+                let Coordinate(_fx, fy) = *from;
+                let mut valid = false;
+
+                if ty > fy && p.color == PieceColor::White {
+                    // White moves down
+                    valid = true;
+                }
+                if ty < fy && p.color == PieceColor::Black {
+                    // Black moves up
+                    valid = true;
+                }
+                if ty > fy && p.color == PieceColor::Black && p.crowned {
+                    // Crowned Black moves down
+                    valid = true;
+                }
+                if ty < fy && p.color == PieceColor::White && p.crowned {
+                    // Crowned White moves up
+                    valid = true;
+                }
+                valid
+            }
+        }
+    }
+
+    fn midpiece_coordinate(&self, x: usize, y: usize, tx: usize, ty: usize) -> Option<Coordinate> {
+        if tx == x + 2 && ty == y + 2 {
+            Some(Coordinate(x + 1, y + 1))
+        } else if x >= 2 && y >= 2 && tx == x - 2 && ty == y - 2 {
+            Some(Coordinate(x - 1, y - 1))
+        } else if x >= 2 && tx == x - 2 && ty == y + 2 {
+            Some(Coordinate(x - 1, y + 1))
+        } else if y >= 2 && tx == x + 2 && ty == y - 2 {
+            Some(Coordinate(x + 1, y - 1))
+        } else {
+            None
+        }
+    }
+
+    fn midpiece(&self, x: usize, y: usize, tx: usize, ty: usize) -> Option<GamePiece> {
+        match self.midpiece_coordinate(x, y, tx, ty) {
+            Some(Coordinate(x, y)) => self.board[x][y],
+            None => None,
+        }
+    }
+
+    fn valid_jump(&self, p: &GamePiece, from: &Coordinate, to: &Coordinate) -> bool {
+        if !to.on_board() || !from.on_board() {
+            false
+        } else {
+            let Coordinate(x, y) = *from;
+            let Coordinate(tx, ty) = *to;
+
+            let midpiece = self.midpiece(x, y, tx, ty);
+            match midpiece {
+                Some(mp) if mp.color != p.color => true,
+                _ => false,
+            }
+        }
+    }
+
+    fn should_crown(&self, piece: GamePiece, coord: Coordinate) -> bool {
+        let Coordinate(_x, y) = coord;
+
+        (y == 0 && piece.color == PieceColor::Black) || (y == 7 && piece.color == PieceColor::White)
+    }
+
+    fn crown_piece(&mut self, coord: Coordinate) -> bool {
+        let Coordinate(x, y) = coord;
+        if let Some(piece) = self.board[x][y] {
+            self.board[x][y] = Some(GamePiece::crowned(piece));
+            true
+        } else {
+            false
+        }
+    }
+
 }
 
 pub struct MoveResult {
