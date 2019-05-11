f9ca61d4b868f561aa5be4b9bf4378fd7473506a
Robert Turner Jr <georob23@gmail.com>
2019-05-10 18:11:00 -0600

    Correct webpack config. Build dist.

--- bindgenhello/webpack.config.js
+++ bindgenhello/webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, dist),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
     },
     plugins: [
