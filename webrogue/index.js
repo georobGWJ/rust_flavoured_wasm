528f23cf9057b18eb03b4a73b28a0202a5801ffe
Robert Turner Jr <georob23@gmail.com>
2019-05-11 20:51:52 -0600

    Implement generateMap method for JS Game class.

--- webrogue/index.js
+++ webrogue/index.js
@@ -21,4 +21,28 @@ var Game = {
         this.rotengine = new ROT.Engine(scheduler);
         this.rotengine.start();
     },
+
+    generateMap: function() {
+        var digger = new ROT.Map.Digger();
+        var freeCells = [];
+
+        var digCallback = function (x, y, value) {
+            if (!value) {
+                var key = x + ", " + y;
+                freeCells.push(key);
+            }
+            this.engine.on_dig(x, y, value);
+        }
+        // Invoke on_dig() on the Rust 'Engine' struct
+        digger.create(digCallback.bind(this));
+
+        this.generateBoxes(freeCells);
+        // Invoke draw_map() on the Rust 'Engine' struct
+        this.engine.draw_map();
+
+        // 'this' refers to the JS Game class instance. _createBeing is a
+        // helper utillity function defined below
+        this.player = this._createBeing(Player, freeCells);
+        this.enemy = this._createBeing(Checko, freeCells);
+    }
 }
