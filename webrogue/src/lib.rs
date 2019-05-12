707233609c9f5e819a5ce49920e376ac8259ebf0
Robert Turner Jr <georob23@gmail.com>
2019-05-11 20:29:53 -0600

    Implement Rust Engine struct and basic methods.

--- webrogue/src/lib.rs
+++ webrogue/src/lib.rs
@@ -29,3 +29,35 @@ extern "C" {
     fn draw_color(this: &Display, x: i32, y: i32, ch: &str, color: &str);
 
 }
+
+#[wasm_bindgen]
+pub struct Engine {
+    display: Display,
+    points: HashMap<GridPoint, String>,
+    prize_location: Option<GridPoint>,
+}
+
+#[wasm_bindgen]
+impl Engine {
+    #[wasm_bindgen(constructor)]
+    pub fn new(display: Display) -> Engine {
+        Engine {
+            display,
+            points: HashMap::new(),
+            prize_location: None,
+        }
+    }
+
+    pub fn on_dig(&mut self, x: i32, y: i32, val: i32) {
+        if val == 0 {
+            let pt = GridPoint { x, y };
+            self.points.insert(pt, ".".to_owned());
+        }
+    }
+
+    pub fn draw_map(&self) {
+        for (k,v) in &self.points {
+            self.display.draw(k.x, k.y, &v);
+        }
+    }
+}
