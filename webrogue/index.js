beac3851baa41a9e7b12fadda1c6420653f6f92f
Robert Turner Jr <georob23@gmail.com>
2019-05-12 20:04:24 -0600

    Add JS Player class and methods.

--- webrogue/index.js
+++ webrogue/index.js
@@ -46,3 +46,50 @@ var Game = {
         this.enemy = this._createBeing(Checko, freeCells);
     }
 }
+
+var Player = function(x, y) {
+    this._core = new PlayerCore(x, y, "@", "#ff0", Game.display);
+    this._core.draw();
+}
+
+Player.prototype.act = function () {
+    Game.rotengine.lock();
+    window.addEventListener("keydown", this);
+}
+
+Player.prototype.handleEvent = function (e) {
+    var keyMap = {};
+    keyMap[38] = 0;  // up arrow
+    keyMap[33] = 1;  // page up
+    keyMap[39] = 2;  // right arrow
+    keyMap[34] = 3;  // page down
+    keyMap[40] = 4;  // down arrow
+    keyMap[35] = 5;  // end
+    keyMap[37] = 6;  // left arrow
+    keyMap[36] = 7;  // home
+
+    var code = e.keyCode;
+
+    if (code == 13 || code == 32) {
+        Game.engine.open_box(this._core, this._core.x(), this._core.y());
+        return;
+    }
+
+    /* numpad directions */
+    if (!(code in keyMap)) { return; }
+
+    /* is there a free space? */
+    var dir = ROT.DIRS[8][keyMap[code]];
+    var newX = this._core.x() + dir[0];
+    var newY = this._core.y() + dir[1];
+
+    if (!Game.engine.free_cell(newX, newY)) { return; }
+
+    Game.engine.move_player(this._core, newX, newY);
+    window.removeEventListener("keydown", this);
+    Game.rotengine.unlock();
+}
+
+Player.prototype.getX = function () { return this._core.x(); }
+
+Player.prototype.getY = function () { return this._core.y(); }
