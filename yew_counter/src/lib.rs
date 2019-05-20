ec09cf24c1196f4d91a9cc62c6a9c7a7c81304e3
Robert Turner Jr <georob23@gmail.com>
2019-05-20 14:48:21 -0600

    Implement Renderable trait functions for Model.

--- yew_counter/src/lib.rs
+++ yew_counter/src/lib.rs
@@ -58,4 +58,18 @@ impl<C> Component<C> for Model
 impl<C> Renderable<C, Model> for Model
     where C: AsMut<ConsoleService> + 'static {
         
+        fn view(&self) -> Html<C, Self> {
+            html! {
+                <div>
+                    <nav class="menu",>
+                        <button onclick=|_| Msg::Increment,> { "Increment" } </button>
+                        <button onclick=|_| Msg::Decrement,> { "Decrement" } </button>
+                        <button onclick=|_| Msg::Bulk(vec![Msg::Increment, Msg::Increment]),> 
+                            { "Increment Twice" } </button>
+                    </nav>
+                    <p>{ self.value }</p>
+                    <p>{ Date::new().to_string() }</p>
+                </div>
+            }
+        }
 }
