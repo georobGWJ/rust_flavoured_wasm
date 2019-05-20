1308cf3952b45e7b68a32ffcc9d6a7268d6e45c7
Robert Turner Jr <georob23@gmail.com>
2019-05-20 14:28:00 -0600

    Implement Component functions for Model.

--- yew_counter/src/lib.rs
+++ yew_counter/src/lib.rs
@@ -1,3 +1,11 @@
+extern crate stdweb;
+#[macro_use]
+extern crate yew;
+
+use stdweb::web::Date;
+use yew::prelude::*;
+use yew::services::console::ConsoleService;
+
 // Models the value of the Counter
 pub struct Model {
     value: i64,
@@ -15,7 +23,32 @@ pub enum Msg {
 // implements the AsMut<ConsoleService> trait.
 impl<C> Component<C> for Model
     where C: AsMut<ConsoleService> {
+        type Message = Msg;
+        type Properties = ();
 
+        fn create(_: Self::Properties, _: &mut Env<C, Self>) -> Self {
+            Model { value: 0, }
+        }
+
+        fn update(&mut self, msg: Self::Message, 
+                  env: &mut Env<C, Self>) -> ShouldRender {
+            match msg {
+                Msg::Increment => {
+                    self.value = self.value + 1;
+                    env.as_mut().log("plus one");
+                },
+                Msg::Decrement => {
+                    self.value = self.value - 1;
+                    env.as_mut().log("minus one");
+                },
+                Msg::Bulk(list) => for msg in list {
+                    self.update(msg, env);
+                    env.as_mut().log("Bulk action");
+                },
+            }
+            // Should this render? For this example, yes. Always.
+            true
+        }
     }
 
 // The Renderable trait allows the Model struct to render anything that is
