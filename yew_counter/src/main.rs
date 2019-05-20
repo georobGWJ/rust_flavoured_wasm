34a7b63911bdfdb16f46df7a3d79384a1e60b993
Robert Turner Jr <georob23@gmail.com>
2019-05-20 13:47:34 -0600

    Implement main.rs logic.

--- yew_counter/src/main.rs
+++ yew_counter/src/main.rs
@@ -1,3 +1,27 @@
+extern crate yew;
+extern crate yew_counter;  // Access to ./src/lib.rs
+
+use yew::prelude::*;
+use yew::services::console::ConsoleService;
+use yew_counter::Model;
+
+pub struct Context {
+    console: ConsoleService,
+}
+
+impl AsMut<ConsoleService> for Context {
+    fn as_mut(&mut self) -> &mut ConsoleService {
+        &mut self.console
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::initialize();
+    let context = Context {
+        console: ConsoleService::new(),
+    };
+
+    let app: App<_, Model> = App::new(context);
+    app.mount_to_body();
+    yew::run_loop();
 }
