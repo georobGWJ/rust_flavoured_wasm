aef89353228a4c74500209375c4ccc9a639ec199
Robert Turner Jr <georob23@gmail.com>
2019-05-21 20:39:41 -0600

    Implement wasm import resolver logic.

--- wasmi_checkers/src/imports.rs
+++ wasmi_checkers/src/imports.rs
@@ -4,4 +4,47 @@ use wasmi::{ Error as InterpreterError, FuncInstance,
 pub const PIECEMOVED_INDEX: usize = 0;
 pub const PIECECROWNED_INDEX: usize = 1;
 
-pub struct RuntimeModuleImportResolver; 
+pub struct RuntimeModuleImportResolver;
+
+impl RuntimeModuleImportResolver {
+
+    pub fn new() -> RuntimeModuleImportResolver {
+        RuntimeModuleImportResolver {}
+    }
+
+}
+
+impl <'a> ModuleImportResolver for RuntimeModuleImportResolver {
+
+    fn resolve_func(&self, field_name: &str, _signature: &Signature)
+                    -> Result<FuncRef, InterpreterError> {
+
+        let func_ref = match field_name {
+            // Provide a FuncRef for the piecemoved() function
+            "piecemoved" => FuncInstance::alloc_host(
+                Signature::new(
+                    &[ 
+                        ValueType: I32, ValueType: I32, 
+                        ValueType: I32, ValueType: I32 
+                    ][..],
+                    None,
+                ),
+                PIECEMOVED_INDEX,
+            ),
+            // Provide a FuncRef for the piececrowned() function
+            "piececrowned" => FuncInstance::alloc_host(
+                Signature::new(
+                    &[ValueType: I32, ValueType: I32,][..], None),
+                PIECECROWNED_INDEX,
+            ),
+            // Return an Error when trying to import an unknown function
+            _ => {
+                return Err(InterpreterError::Function(format!(
+                    "host module does not export function: {}", field_name
+                )))
+            }
+        };
+        Ok(func_ref)
+    }
+
+}
