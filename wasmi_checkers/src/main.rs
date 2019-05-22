2ecf54e72c8ee2bceadd86f0e115ebcad5f310f7
Robert Turner Jr <georob23@gmail.com>
2019-05-21 21:59:08 -0600

    Implement Rust host runtime.

--- wasmi_checkers/src/main.rs
+++ wasmi_checkers/src/main.rs
@@ -1,3 +1,9 @@
+// A Pattern for creating a Rust wasm-host
+//  1)  Create an imports resolver that provides a signature and invocation index for
+//      each imported function.    See imports.rs
+//  2)  Create a Runtime for Externals.    See runtime.rs
+//  3)  Create API Wrapper for exported functions
+
 fn main() {
     println!("Hello, world!");
 }
