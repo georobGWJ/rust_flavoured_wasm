a308bb8ac0d567d1a44767ea272e7b5c9c2bbcfd
Robert Turner Jr <georob23@gmail.com>
2019-05-21 20:08:45 -0600

    Add logic to call WASM function, get and display results.

--- wasmi_add/src/main.rs
+++ wasmi_add/src/main.rs
@@ -12,8 +12,35 @@ fn main() -> Result<(), Box<Error>>{
     // Read in the wasm bytes
     let mut buffer = Vec::new();
     {
-        let mut f = File::open("../fundamentals/add.wasm")?; // Sloppy error handling
+        // add.wasm was in our raw_wasm project
+        let mut f = File::open("../raw_wasm/add.wasm")?;
         f.read_to_end(&mut buffer)?;
     }
     let module = wasmi::Module::from_buffer(buffer)?;
+
+    // Create a new WASM module with default imports
+    let instance = ModuleInstance::new( &module, &ImportsBuilder::default())
+        .expect("Failed to instantiate WASM module...")
+        .assert_no_start();
+
+    let mut args = Vec::<RuntimeValue>::new();
+    args.push(RuntimeValue::from(28));
+    args.push(RuntimeValue::from(14));
+
+    let result: Option<RuntimeValue> = 
+        instance.invoke_export("add", &args, &mut NopExternals)?;
+
+    match result {
+        Some(RuntimeValue::I32(v)) => {
+            println!("The answer to your question is {}", v);
+        },
+        Some(_) => {
+            println!("What? What is that data type?!?");
+        },
+        None => {
+            println!("Failed to get a result from the invocation!");
+        },
+    }
+    Ok(())
+
 }
