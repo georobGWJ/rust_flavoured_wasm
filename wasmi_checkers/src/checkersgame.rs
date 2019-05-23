1bbf05bfae48fe01203593bbc03923147f6968c2
Robert Turner Jr <georob23@gmail.com>
2019-05-22 19:52:31 -0600

    Add helper functions to API.

--- wasmi_checkers/src/checkersgame.rs
+++ wasmi_checkers/src/checkersgame.rs
@@ -36,14 +36,14 @@ impl CheckersGame {
         }
     }
 
-    // 1
+    // Wrapper for the initBoard() module function
     pub fn init(&mut self) -> Result<()> {
         self.module_instance
             .invoke_export("initBoard", &[], &mut self.runtime)?;
         OK(())
     }
 
-    // 2
+    // Call move using simple i32 coord values
     pub fn move_piece(&mut self, from: &Coordinate, to: &Coordinate) -> Result<bool> {
         let res = self.module_instance.invoke_export( "move",
             &[
@@ -64,7 +64,7 @@ impl CheckersGame {
         }
     }
 
-    // 3
+    // Get and convert a numeric variable into an PieceColor Enum
     pub fn get_turn_owner(&mut self) -> Result<PieceColor> {
         let res = self
             .module_instance
@@ -78,8 +78,62 @@ impl CheckersGame {
             _ => Err(From::from("Bad invocation.")),
         }
     }
+
+    // We can access the raw bytes in memory that represent the
+    // board state and convert it into a string.
+    pub fn get_board_contents(&mut self) -> Result<String> {
+        let export = self.module_instance.export_by_name("memory");
+
+        let header = r#"
+  0   1   2   3   4   5   6   7
+.---.---.---.---.---.---.---.---."#;
+        let footer = "  `---^---^---^---^---^---^---^---^";
+
+        let middle_string = match export {
+            Some(ExternVal::Memory(mr)) => gen_board(&mr),
+            _ => " -- no board data found --".to_string(),
+        };
+        Ok(format!("{}\n{}\n{}"), header, middle_string, footer))
+    }
 }
 
+
+// Get a String that 'draws' the board
+fn gen_board(memory: &MemoryRef) -> String {
+    let mut vals = Vec::<String>::new();
+
+    for y in 0..8 {
+        vals.push(format!("{}", y));
+        for x in 0..8 {
+            let offset = calc_offset(x, y);
+            let bytevec: Vec<u8> = memory.get(offset, 4).unwrap();
+            let value = to_u32(&bytevec[..]);
+
+            vals.push(format!("|{}", value_label(value)));
+        }
+        vals.push("|\n".into());
+    }
+    vals.join("")
+}
+
+fn value_label(v: u32) -> String {
+    match v {
+        0 => "   ",
+        1 => " B ",
+        2 => " W ",
+        5 => "*B*",
+        6 => "*W*",
+        _ => "???",
+    }.into()
+}
+
+fn to_u32(bytes: &[u8]) -> u32 {
+    bytes.iter().rev().fold(0, |acc, &b| acc * 2 + b as u32)
+}
+
+fn calc_offset(x: usize, y: usize) -> u32 {
+    ((x + y * 8) * 4) as u32
+}
 
 
 // This function takes any struct implementing the ModuleImportResolver trait.
