4f8c094cf89ab6281ec24df29674e3e2e42adfdd
Robert Turner Jr <georob23@gmail.com>
2019-05-26 18:06:54 -0600

    Add requirements/imports to pi_host main.

--- pi_gims/pi_host/src/main.rs
+++ pi_gims/pi_host/src/main.rs
@@ -1,3 +1,28 @@
+#[cfg(any(target_arch = "armv7", target_arch = "arm"))]
+extern crate blinkt;
+
+extern crate ctrlc;
+extern crate notify;
+extern crate wasmi;
+
+use notify::{ DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher };
+use std::path::Path;
+use std::sync::mpsc::{ channel, RecvTimeoutError, Sender };
+use std::thread;
+use std::time::Duration;
+use wasm::Runtime;
+use wasmi::RuntimeValue;
+
+const MODULE_FILE: &'static str = 
+    "/Users/georob/Desktop/spike/wasm/rust_flavoured_wasm/pi_gims/indicator.wasm";
+const MODULE_DIR: &'static str = 
+    "/Users/georob/Desktop/spike/wasm/rust_flavoured_wasm/pi_gims/";
+
+enum RunnerCommand {
+    Reload,
+    Stop,
+}
+
 fn main() {
     println!("Hello, world!");
 }
