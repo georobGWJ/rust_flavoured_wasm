d85c63bdae6db4325c35786d618f5ad5def8bc9c
Robert Turner Jr <georob23@gmail.com>
2019-06-06 21:27:03 -0600

    Add comments. Remove unused logic.

--- waros/warsdk/src/lib.rs
+++ waros/warsdk/src/lib.rs
@@ -1,60 +1,76 @@
+// Scan in a particular direction and if another bot is detected, return the
+// range. Otherwise return 0
 pub fn scan(angle:i32, resolution: i32) -> i32{
     unsafe { ffi::scan(angle, resolution) }
 }
 
+// Fire the cannon at a given degree and range (max 700 m). Retutn 0 if no
+// shot fired or 1 if a shot was fired.
 pub fn cannon(angle: i32, range: i32) -> i32{
     unsafe { ffi::cannon(angle, range) }
 }
 
+// Move the bot in a given direction at a given speed percentage
 pub fn drive(angle: i32, speed: i32) -> i32{
     unsafe { ffi::drive(angle, speed) }
 }
 
+// Return the percentage of damage currently taken by the bot
 pub fn damage() -> i32{
     unsafe { ffi::damage() }
 }
 
+// Return the percentage speed of the bot 
 pub fn speed() -> i32{
     unsafe { ffi::speed() }
 }
 
+// Return the current x coordinate of the bot
 pub fn loc_x() -> i32{
     unsafe { ffi::loc_x() }
 }
 
+// Return the current y coordinate of the bot
 pub fn loc_y() -> i32{
     unsafe { ffi::loc_y() }
 }
 
+// Return a random number between 0 and a limit (max 32,767)
 pub fn rand(limit: i32) -> i32{
     unsafe { ffi::rand(limit) }
 }
 
+// Return the square root of a number, coerced to positive if needed
 pub fn wsqrt(number: i32) -> i32{
     unsafe { ffi::wsqrt(number) }
 }
 
+// Trig sin() renamed to avoid namespace pollution
 pub fn wsin(degree: i32) -> i32{
     unsafe { ffi::wsin(degree) }
 }
 
+// Trig cos() renamed to avoid namespace pollution
 pub fn wcos(degree: i32) -> i32{
     unsafe { ffi::wcos(degree) }
 }
 
+// Trig tan() renamed to avoid namespace pollution
 pub fn wtan(degree: i32) -> i32{
     unsafe { ffi::wtan(degree) }
 }
 
+// Trig arctan() renamed to avoid namespace pollution
 pub fn watan(degree: i32) -> i32{
     unsafe { ffi::watan(degree) }
 }
 
+// Determine the direction to a specified x and y
 pub fn plot_course(tx: i32, ty: i32) -> i32{
     unsafe { ffi::plot_course(tx, ty) }
 }
 
-// Utility example fir moving to a destination and stopping
+// Utility example for moving to a destination and stopping
 // This logic does NOT recover from a collision en route
 pub fn go(target_x: i32, target_y:i32) {
     let course = plot_course(target_x, target_y);
@@ -73,12 +89,13 @@ pub fn go(target_x: i32, target_y:i32) {
     }
 }   
 
-// Counterclockwise azimuths why?
+// Counterclockwise azimuths to match original Crobots logic
 pub const ANGLE_EAST: i32 = 0;
 pub const ANGLE_NORTH: i32 = 90;
 pub const ANGLE_WEST: i32 = 180;
 pub const ANGLE_SOUTH: i32 = 270;
 
+// Other constants bots can use
 pub const MAX_X: u32 = 1000;
 pub const MAX_Y: u32 = 1000;
 
@@ -92,10 +109,3 @@ pub const PROJECTILE_MAX_RANGE: i32 = 200;
 
 mod ffi;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
